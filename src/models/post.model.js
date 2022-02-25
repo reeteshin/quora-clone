666abe259afccb2d5a0cd2efa2d77470a91b6e1c
reetesh meena <reeteshmeena73@gmail.com>
2022-02-25 12:54:50 +0530

    Update post.model.js

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -6,9 +6,17 @@ const postSchema = new mongoose.Schema({
         ref:"user",
         required: true
     },
+    answer_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"answer",
+        required: true
+    },
+    
     post_que: {type: String, required: false},
     post_dec: {type: String, required: true},
-    image: {type:String, required: false}
+    image: {type:String, required: false},
+    
+
 },{
     versionKey: false,
     timestamps: true
