e2f59fdbb7e800812f5ef12b84650b7d37af4a11
reetesh meena <reeteshmeena73@gmail.com>
2022-02-25 12:18:00 +0530

    Update post.controller.js

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -4,7 +4,20 @@ const Post = require("../models/post.model");
 
 const crudController = require("./crud.controllers");
 
-router.get("", crudController(Post).get);
+router.get("", async(req, res) => {
+    try{
+        const post = await Post.find()
+        .populate({path: "user_id", select: { first_name: 1}})
+        
+        .lean().exec();
+      return   res.status(200).send(post);
+    }catch(err){
+    return  res.status(500).send({error: err.message});
+    }
+})
+
+
+//router.get("", crudController(Post).get);
 router.get("/:id", crudController(Post).getOne);
 router.post("", crudController(Post).post);
 router.patch("/:id", crudController(Post).updateOne);
