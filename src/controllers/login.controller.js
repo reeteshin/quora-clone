635e1b1b9a4517b336b82135bfa28b4b2d310552
reetesh meena <reeteshmeena73@gmail.com>
2022-02-27 14:14:38 +0530

    login update

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -84,4 +84,103 @@ Router.post("/signin",(req,res)=>{
     })
 })
 
+router.post("/refreshtoken",async (req,res)=>{
+    try {
+        const rf_token = req.cookies.refreshtoken
+        if(!rf_token) return res.status(400).json({error: "Please login now!"})
+
+        jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+            if(err) return res.status(400).json({error: "Please login now!"})
+
+            const access_token = createAccessToken({id: user.id})
+            res.json({access_token})
+        })
+    } catch (err) {
+        return res.status(500).json({error: err.message})
+    }
+})
+router.get("/userInfo",auth,async (req,res)=>{
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+
+        res.json(user)
+    } catch (err) {
+        return res.status(500).json({error: err.message})
+    }
+})
+
+
+router.get("/logout",(req,res)=>{
+    try {
+        res.clearCookie('refreshtoken', {path: '/users/refresh_token'})
+        return res.json({message: "Logged out."})
+    } catch (err) {
+        return res.status(500).json({message: err.message})
+    }
+})
+
+
+router.post("/googlelogin",async (req,res)=>{
+    try {
+        const {tokenId} = req.body
+
+        
+        
+        const { email, name} = verify.payload
+
+        const password = email + process.env.GOOGLE_SECRET
+
+        const passwordHash = await bcrypt.hash(password, 12)
+
+       
+
+        const user = await User.findOne({email:email})
+
+        if(user){
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "Password is incorrect."})
+
+            const refresh_token = createRefreshToken({id: user._id})
+            res.cookie('refreshtoken', refresh_token, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7*24*60*60*1000 // 7 days
+            })
+
+            res.json({msg: "Login success!"})
+        }else{
+            const newUser = new User({
+                firstname:name, email, password: passwordHash
+            })
+
+            await newUser.save()
+            
+            const refresh_token = createRefreshToken({id: newUser._id})
+            res.cookie('refreshtoken', refresh_token, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7*24*60*60*1000 // 7 days
+            })
+
+            res.json({msg: "Login success!"})
+        }
+
+
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+})
+
+
+const createActivationToken = (payload) => {
+    return jwt.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, {expiresIn: '15m'})
+}
+const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '45m'})
+}
+
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
+}
+
 module.exports = Router;
--- src/models/login.model.js
+++ src/models/login.model.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const userSchema=new mongoose.Schema({
     
-    email:{type:String,required:true},
-    password:{type:String,required:true}
+    email:{type:String, required:true,unique:true},
+    password:{type:String, required:true}
+
 },{
-    versionkey:false,
-    timeStamps:true
+    versionKey:false,
+    timestamps:true
 })
-module.exports =mongoose.model("UserLogin",userSchema)
+module.exports=mongoose.model("UserLogin",userSchema)
